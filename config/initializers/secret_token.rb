850fabe2becfa34a88fad1ad6fb6b8b2c6d29563
Tsyril <cmulette@yahoo.fr>
2014-03-30 14:31:27 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Garcimore::Application.config.secret_key_base = 'cf95ab9096e7ca83554962ed3ae55a7d61cb4e69e0356b7cf932e96017d94a38ae44291b463278f94977dc10f5475a2d013a9ecba4ad2dbf143b97d76f28eee3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Garcimore::Application.config.session_store :cookie_store, key: '_Garcimore_session'
